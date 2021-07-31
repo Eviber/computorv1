c6b4e27df366aebf50a04f9e3ea5c1e21ad19a00
Eviber <youv.didj@gmail.com>
2021-07-31 02:14:09 +0200

    better random polynome generation

--- randompoly.py
+++ randompoly.py
@@ -2,15 +2,16 @@
 
 from random import randint
 
-def r():
-    return (randint(-5000, 5000)/100)
+def r(d = 0):
+    d = pow(10, d)
+    return (randint(-5*d, 5*d)/1*d)
 
 def randompoly():
     a = r()
     b = r()
     c = r()
-    s = f"{a}X^2 " + (f"- {-b}" if (b < 0) else f"+ {b}") + "X "
-    s = s + (f"- {-c}" if (c < 0) else f"+ {c}")
+    s = f"{a:g}X^2 " + (f"- {-b:g}" if (b < 0) else f"+ {b:g}") + "X "
+    s = s + (f"- {-c:g}" if (c < 0) else f"+ {c:g}")
     return (s)
 
 def main():
