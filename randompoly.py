bb78b5f9df9e7922b12a7617c306362191781c41
Eviber <youv.didj@gmail.com>
2021-08-23 17:41:06 +0200

    reformatting

--- randompoly.py
+++ randompoly.py
@@ -2,9 +2,11 @@
 
 from random import randint
 
-def r(d = 0):
+
+def r(d=0):
     d = pow(10, d)
-    return (randint(-5*d, 5*d)/1*d)
+    return randint(-5 * d, 5 * d) / 1 * d
+
 
 def randompoly():
     a = r()
@@ -12,10 +14,12 @@ def randompoly():
     c = r()
     s = f"{a:g}X^2 " + (f"- {-b:g}" if (b < 0) else f"+ {b:g}") + "X "
     s = s + (f"- {-c:g}" if (c < 0) else f"+ {c:g}")
-    return (s)
+    return s
+
 
 def main():
     print(randompoly() + " = " + randompoly())
+
 
 if __name__ == "__main__":
     main()
